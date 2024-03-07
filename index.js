f5cd61b22172cbc28cb77ad43335de562d40f3ee
assilbekov <askhat.assilbekov@gmail.com>
2024-03-06 22:30:00 -0300

    write to supabase

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 import fs from "fs"
 
 fs.readFile('scrimba-info.txt', 'utf8', async (err, data) => {
@@ -13,6 +16,23 @@ fs.readFile('scrimba-info.txt', 'utf8', async (err, data) => {
     chunkOverlap: 50,
   })
   const output = await splitter.createDocuments([data])
+
+  const sbApiKey = process.env.SUPABASE_API_KEY
+  const sbUrl = process.env.SUPABASE_URL
+  const openAIApiKey = process.env.OPENAI_API_KEY
+
+  console.log({sbApiKey, sbUrl, openAIApiKey})
+
+  const client = createClient(sbUrl, sbApiKey)
+
+  await SupabaseVectorStore.fromDocuments(
+    output,
+    new OpenAIEmbeddings({ openAIApiKey }),
+    {
+      client,
+      tableName: 'documents',
+    }
+  )
 
   fs.writeFile('output.json', JSON.stringify(output, null, 2), (err) => {
     if (err) {
