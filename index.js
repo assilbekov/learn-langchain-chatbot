5b124e773e35c197f49eaffad86c07719427ae31
assilbekov <askhat.assilbekov@gmail.com>
2024-03-06 22:08:47 -0300

    refactor to use fs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-try {
-  const result = await fetch('scrimba-info.txt')
-  const text = await result.text()
-} catch (err) {
-  console.log(err)
-}
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import fs from "fs"
+
+fs.readFile('scrimba-info.txt', 'utf8', async (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const splitter = new RecursiveCharacterTextSplitter()
+  const output = await splitter.createDocuments([data])
+  console.log(output)
+})
